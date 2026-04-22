bac3f01d03817122cd0536ed832d5c5d601b2f90
donaldzhu <donaldzhu.toronto@gmail.com>
2026-04-21 22:35:23 -0400

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  let { children } = $props()
+  import Header from "./_components/header.svelte"
+</script>
+
+{@render children()}
+
+<Header />
---
+++ src/routes/_components/header.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<header>
+  <nav>
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+    </ul>
+  </nav>
+</header>
